4759ab994f427df9f003ed4803c4e8c3c95be6a8
Luisszzuniga <l.zuniga@webandcow.com>
2023-01-10 14:04:16 +0100

    test

--- server_deploy.sh
+++ server_deploy.sh
@@ -3,31 +3,27 @@ set -e
  
 echo "Deploying application ..."
  
-# Enter maintenance mode
-(php artisan down) || true
-    # Update codebase
-    git fetch origin main
-    git reset --hard origin/main
- 
-    # Install dependencies based on lock file
-    composer install --no-interaction --prefer-dist --optimize-autoloader
+# Update codebase
+git fetch origin main
+git reset --hard origin/main
 
-    # Install npm and build
-    npm i
-    npm run build
- 
-    # Migrate database
-    php artisan migrate --force
- 
-    # Note: If you're using queue workers, this is the place to restart them.
-    # ...
- 
-    # Clear cache
-    php artisan optimize
- 
-    # Reload PHP to update opcache
-    echo "" | sudo -S service php7.4-fpm reload
-# Exit maintenance mode
-php artisan up
+# Install dependencies based on lock file
+composer install --no-interaction --prefer-dist --optimize-autoloader
+
+# Install npm and build
+npm i
+npm run build
+
+# Migrate database
+php artisan migrate --force
+
+# Note: If you're using queue workers, this is the place to restart them.
+# ...
+
+# Clear cache
+php artisan optimize
+
+# Reload PHP to update opcache
+echo "" | sudo -S service php7.4-fpm reload
  
 echo "Application deployed!"
